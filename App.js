2691f3ee06c682d802c2469456096d6b1d42884c
rhobbikhaidir <rhobbie09@gmail.com>
2020-09-15 18:18:44 +0700

    saya membuat textInput menjadi SearchBar cukup menarik dan kabar baik nya saya juga dapet materi react native maximilan udemy yeay

--- App.js
+++ App.js
@@ -13,43 +13,63 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
   Image,
+  TextInput,
 } from 'react-native';
 
 import {Account, Rumah, Order, Help, Chatting} from './icons';
+import search from './icons/Search.png'
+
+
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      {/* <Text>CloneGojek</Text> */}
-      <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+      <View style={{flex: 1, backgroundColor: 'pink'}}>
+        <View>
+          <View style={{position: 'relative'}}>
+            <TextInput
+              placeholder="What do you want to eat?"
+              style={{
+                borderWidth: 1,
+                borderRadius: 25,
+                fontSize: 13,
+                paddingLeft: 45,
+                paddingRight: 20,
+                top: 10,
+              }}
+            />
+            <Image source={search} style={{position: 'absolute', top: 23, left: 10}}/>
+          </View>
+          <View></View>
+        </View>
+      </View>
       <View style={{height: 53, flexDirection: 'row'}}>
-        <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center', marginTop: 5}}>
           <Rumah width={26} height={26} />
           <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
             Home
           </Text>
         </View>
-        <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center', marginTop: 5}}>
           <Order width={26} height={26} />
           <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
             Orders
           </Text>
         </View>
-        <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center', marginTop: 5}}>
           <Help width={26} height={26} />
           <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
             Help
           </Text>
         </View>
-        <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center', marginTop: 5}}>
           <Chatting width={26} height={26} />
           <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
             Inbox
           </Text>
         </View>
-        <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center', marginTop: 5}}>
           <Account width={26} height={26} />
           <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
             Account
