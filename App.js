a66bfec79eb385f8b9dd797d728f8d4b604d3b41
rhobbikhaidir <rhobbie09@gmail.com>
2020-09-16 10:42:02 +0700

    saya membuat main features di materi hari ini ini cukup menyenangkan menurut saya sendiri awkwkkws tetap semnagat for better future my self

--- App.js
+++ App.js
@@ -17,17 +17,29 @@ import {
   TextInput,
 } from 'react-native';
 
-import {Account, Rumah, Order, Help, Chatting} from './icons';
-import search from './icons/Search.png'
-
-
+import {
+  Account,
+  Rumah,
+  Order,
+  Help,
+  Chatting,
+  RumahActive,
+  Nearbly,
+  TopUp,
+  More,
+  PayFeatures,
+  QrCode, GoRide, GoCar, GoBlueBird, GoSend, GoDeals, GoPulsa, GoFood, GoMore
+} from './icons';
+import search from './icons/Search.png';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'pink'}}>
-        <View>
-          <View style={{position: 'relative'}}>
+      <View style={{flex: 1}}>
+        {/* Search Bar */}
+        <View
+          style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+          <View style={{position: 'relative', flex: 1}}>
             <TextInput
               placeholder="What do you want to eat?"
               style={{
@@ -37,17 +49,252 @@ const App = () => {
                 paddingLeft: 45,
                 paddingRight: 20,
                 top: 10,
+                backgroundColor: 'white',
+                marginRight: 18,
               }}
             />
-            <Image source={search} style={{position: 'absolute', top: 23, left: 10}}/>
+            <Image
+              source={search}
+              style={{position: 'absolute', top: 23, left: 10}}
+            />
           </View>
-          <View></View>
+          <View
+            style={{width: 35, alignItems: 'center', justifyContent: 'center'}}>
+            <QrCode width={25} height={25} />
+          </View>
+        </View>
+        {/*Gopay*/}
+        <View style={{marginHorizontal: 17, marginTop: 20}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              backgroundColor: '#2c5fb8',
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+              padding: 14,
+            }}>
+            <Text style={{color: 'white', fontSize: 14, fontWeight: 'bold'}}>
+              GOPAY
+            </Text>
+            <Text style={{color: 'white', fontSize: 17, fontWeight: 'bold'}}>
+              Rp 50.000
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingTop: 20,
+              paddingBottom: 14,
+              backgroundColor: '#2f65bd',
+              borderBottomLeftRadius: 10,
+              borderBottomRightRadius: 10,
+            }}>
+            <View style={{flex: 1, alignItems: 'center'}}>
+              <PayFeatures width={40} height={40} />
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  marginTop: 15,
+                }}>
+                Pay
+              </Text>
+            </View>
+            <View style={{flex: 1, alignItems: 'center'}}>
+              <Nearbly width={40} height={40} />
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  marginTop: 15,
+                }}>
+                Nearbly
+              </Text>
+            </View>
+            <View style={{flex: 1, alignItems: 'center'}}>
+              <TopUp width={40} height={40} />
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  marginTop: 15,
+                }}>
+                Top Up
+              </Text>
+            </View>
+            <View style={{flex: 1, alignItems: 'center'}}>
+              <More width={40} height={40} />
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  marginTop: 15,
+                }}>
+                More
+              </Text>
+            </View>
+          </View>
+        </View>
+        {/* main features */}
+        <View
+          style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            marginHorizontal: 16,
+            marginTop: 18,
+          }}>
+          <View
+            style={{
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              width: '100%',
+              marginBottom: 18,
+            }}>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoRide width={40} height={40} />
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>GO-RIDE </Text>
+            </View>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoCar width={40} height={40} />
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>GO-CAR</Text>
+            </View>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoBlueBird width={40} height={40}/>
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>GO-BLUEBIRD</Text>
+            </View>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoSend  width={40} height={40}/>
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>GO-SEND</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              width: '100%',
+            }}>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoDeals width={40} height={40}/>
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>GO-DEALS</Text>
+            </View>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoPulsa width={40} height={40}/>
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>GO-PULSA</Text>
+            </View>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoFood  width={40} height={40}/>
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>GO-FOOD</Text>
+            </View>
+            <View>
+              <View
+                style={{
+                  width: 58,
+                  height: 58,
+                  borderWidth: 1,
+                  borderColor: '#efefef',
+                  borderRadius: 18,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <GoMore width={40} height={40}/>
+                </View>
+              <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 7}}>MORE</Text>
+            </View>
+          </View>
         </View>
       </View>
       <View style={{height: 53, flexDirection: 'row'}}>
         <View style={{flex: 1, alignItems: 'center', marginTop: 5}}>
-          <Rumah width={26} height={26} />
-          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+          <RumahActive width={26} height={26} />
+          <Text
+            style={{
+              fontSize: 10,
+              color: '#545454',
+              marginTop: 4,
+              color: '#43ab4a',
+            }}>
             Home
           </Text>
         </View>
@@ -58,7 +305,7 @@ const App = () => {
           </Text>
         </View>
         <View style={{flex: 1, alignItems: 'center', marginTop: 5}}>
-          <Help width={26} height={26} />
+          <Help />
           <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
             Help
           </Text>
--- icons/index.js
+++ icons/index.js
@@ -8,9 +8,37 @@ import Help from './live.svg';
 import HelpActive from './live-aktif.svg';
 import Chatting from './chating.svg';
 import ChattingActive from './chating-aktif.svg';
+import Promo from './Promo.svg';
+import Hot from './Hot.svg';
+import Nearbly from './nearbly.svg';
+import PayFeatures from './play.svg';
+import TopUp from './topup.svg';
+import More from './more.svg';
+import QrCode from './qrcode.svg';
+import GoMore from './gomore.svg';
+import GoBlueBird from './gobluebird.svg';
+import GoCar from './gocar.svg';
+import GoDeals from './godeals.svg';
+import GoFood from './gofood.svg';
+import GoPulsa from './gopulsa.svg';
+import GoRide from './goride.svg';
+import GoSend from './gosend.svg';
 
 export {
   Account,
+  QrCode,
+  GoMore,
+  GoBlueBird,
+  GoCar,
+  GoDeals,
+  GoFood,
+  GoPulsa,
+  GoRide, 
+  GoSend,
+  Nearbly,
+  PayFeatures,
+  TopUp,
+  More,
   AccountActive,
   Rumah,
   RumahActive,
@@ -20,4 +48,6 @@ export {
   HelpActive,
   Chatting,
   ChattingActive,
+  Promo,
+  Hot,
 };
